f6799b1cdb853667c1184e2c28f7f6ab0db75e44
Stephen Quick <quick.stephen@gmail.com>
2016-03-16 15:17:17 -0700

    Use controller based variable to for hamburger menu.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -3,6 +3,7 @@ export default class AppComponent {
     constructor() {
         this.name = 'Stephen Quick';
         this.year = new Date().getFullYear();
+        this.navCollapsed = true;
     }
 
 }
