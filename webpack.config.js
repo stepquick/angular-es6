c92150952eb7d66bb86dbe5c913b9150c7e15cad
Stephen Quick <quick.stephen@gmail.com>
2016-03-16 16:54:53 -0700

    Clean up webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,17 @@ var OpenBrowserWebpackPlugin = require('open-browser-webpack-plugin');
 module.exports = {
     devtool: 'inline-source-map',
     debug: true,
-    entry: {
-        main: './src/main.js',
-        vendor: ['angular', 'angular-ui-bootstrap']
-    },
+    entry: ['./src/main.js'],
     output: {
         filename: '[name].[hash].js',
         sourceMapFilename: '[name].[hash].map'
     },
     module: {
         loaders: [
-          { test: /\.js$/, loader: 'babel', exclude: /(\.test.js$|node_modules)/ },
-          { test: /\.css$/, loader: 'style!css' },
-          { test: /\.tpl.html/, loader: 'html' },
-          { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-          { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
-          { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-          { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+          {test: /\.js$/, loader: 'babel', exclude: /(\.test.js$|node_modules)/},
+          {test: /\.css$/, loader: 'style!css'},
+          {test: /\.tpl.html/, loader: 'html'},
+          {test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, loader: 'url?limit=50000'}
         ]
     },
     plugins: [
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,38 +2,29 @@ var webpack = require('webpack');
 
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
-var OpenBrowserWebpackPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
     resolve: {
         extensions: ['', '.js', '.tpl.html']
     },
-    entry: {
-        main: './src/main.js',
-        vendor: ['angular', 'angular-ui-bootstrap']
-    },
+    entry: ['./src/main.js'],
     output: {
         path: './dist',
         filename: '[name].[hash].js',
         sourceMapFilename: '[name].[hash].map',
-        publicPath: '/angular1.50-es6-webpack/'
     },
     module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel', exclude: /(\.test.js$|node_modules)/ },
-            { test: /\.css$/, loader: 'style!css' },
-            { test: /\.tpl.html/, loader: 'html' },
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
-        ]
+      loaders: [
+        {test: /\.js$/, loader: 'babel', exclude: /(\.test.js$|node_modules)/},
+        {test: /\.css$/, loader: 'style!css'},
+        {test: /\.tpl.html/, loader: 'html'},
+        {test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, loader: 'url?limit=50000'}
+      ]
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({ template: './src/index.html' }),
         new webpack.optimize.DedupePlugin(),
-        new CleanWebpackPlugin(['dist']),
-        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: '[name].[hash].js', minChunks: Infinity }),
         new webpack.optimize.UglifyJsPlugin({
             mangle: false
         })
