59b65cf15d14dfa0ee924ce58ef36b79f493dde6
Stephen Quick <quick.stephen@gmail.com>
2016-03-16 09:59:04 -0700

    Move name to bottom of page.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,7 +1,7 @@
 export default class AppComponent {
 
     constructor() {
-        this.name = 'Stephen';
+        this.name = 'Stephen Quick';
         this.year = new Date().getFullYear();
     }
 
