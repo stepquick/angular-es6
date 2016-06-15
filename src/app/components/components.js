16ca26c1ca80cadea2fb349cb30f4d83ad5bf432
Stephen Quick <quick.stephen@gmail.com>
2016-06-15 10:09:23 -0700

    Restructure components to use index to import whole directory.

--- src/app/components/components.js
+++ src/app/components/components.js
@@ -1,8 +1,8 @@
 import angular from 'angular';
 
-import home from './home/home';
-import about from './about/about';
-import test from './test/test';
+import home from './home';
+import about from './about';
+import test from './test';
 
 const components = angular
 	.module('main.app.components', [home, about, test])
