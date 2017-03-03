c637e69c5c9029641b480d82fce9c7055f0b6705
Stephen Quick <squick2@emory.edu>
2017-03-03 11:37:37 -0500

    Emo text is gone.

--- src/app/components/blog/blog.service.js
+++ src/app/components/blog/blog.service.js
@@ -1,10 +1,10 @@
 export class BlogService {
   	constructor() {
         this.posts = [
-            {title: "This is going to hurt you.", date: "2016-09-20T13:28:06.419Z", body: "Why are you running away?"},
-            {title: "They say what doesn't kill you.", date: "2016-10-15T13:28:06.419Z", body: "You really don't get it do you?"},
-            {title: "If you could shut up for a second.", date: "2016-12-08T13:28:06.419Z", body: "It hurts."},
-            {title: "Please think about what you're doing.", date: "2016-01-21T13:28:06.419Z", body: "It's crushing me."}
+            {title: "Hey there", date: "2016-09-20T13:28:06.419Z", body: "Lorem ipsum dolor sit amet, te hinc accumsan sea, albucius philosophia et has, eu his iriure fastidii. Tritani ornatus ei eos, mel erat discere ea, dicam repudiandae ut nec. Cu eum mutat adversarium, ex eum fastidii theophrastus. Ancillae periculis omittantur per te, per in suas phaedrum, audiam copiosae ea mei. In nemore mollis his, mei ex sonet nominavi necessitatibus."},
+            {title: "They.", date: "2016-10-15T13:28:06.419Z", body: "Scripta eruditi ne sed. Sit docendi denique definitionem cu, ne docendi salutandi intellegat vel. Ea mei noster cotidieque, ius adhuc ipsum graeci id."},
+            {title: "If that.", date: "2016-12-08T13:28:06.419Z", body: "Eu etiam nullam quidam vim, nec nihil mentitum accommodare in. Eu scripta reformidans eos. Nam id epicurei iudicabit deseruisse, facilisi ocurreret te has. Vis accusata platonem ei. Ne vix voluptua constituam comprehensam, et illum dicant iudicabit has."},
+            {title: "Please think.", date: "2016-01-21T13:28:06.419Z", body: "Ne conceptam sadipscing complectitur vis, docendi constituto persequeris at vel. Omnes accumsan vix ut. Hendrerit contentiones eum in, qui ea nobis noster officiis, nibh habemus democritum nec ex. No sed suas fastidii legendos, at mea legimus molestie noluisse."}
         ];
   	}
     all() {
